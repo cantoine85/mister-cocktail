4909259512728f5e20070f9c8375de022d6e9441
Claire Antoine <cantoine85@gmail.com>
2015-08-20 15:48:56 +0200

    adding index action and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
+
+Cocktail.create(name:"mojito")
+Cocktail.create(name:"caipirina")
+Cocktail.create(name: "gin fizz")
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
