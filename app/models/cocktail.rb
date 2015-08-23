7e7d9f49008dc2d83b1fc1bcfc553602a3d518d3
Claire Antoine <cantoine85@gmail.com>
2015-08-23 23:49:17 +0200

    adding image upload

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,18 @@
 class Cocktail < ActiveRecord::Base
+
+  # Associations
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+
+  # Validations
   validates :name, presence: true
   validates :name, uniqueness: true
+
+  # Paperclip : Add picture to a cocktail
+  has_attached_file :picture,
+    styles: { medium: "500x500>", thumb: "100x100>" }
+
+  # Validations for attachment content type
+  validates_attachment_content_type :picture,
+    content_type: /\Aimage\/.*\z/
 end
